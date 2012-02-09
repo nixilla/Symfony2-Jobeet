8a4fc25a69225157d0056bbbc7c5748a93050083
Janusz Slota <janusz.slota@nixilla.com>
2012-02-09 16:08:58 +0000

    Added JobeetJob model

---
+++ src/Nixilla/JobeetBundle/Entity/JobeetJob.php
@@ -0,0 +1,352 @@
+<?php
+
+namespace Nixilla\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Nixilla\JobeetBundle\Entity\JobeetJob
+ */
+class JobeetJob
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $type
+     */
+    private $type;
+
+    /**
+     * @var string $company
+     */
+    private $company;
+
+    /**
+     * @var string $logo
+     */
+    private $logo;
+
+    /**
+     * @var string $url
+     */
+    private $url;
+
+    /**
+     * @var string $position
+     */
+    private $position;
+
+    /**
+     * @var string $location
+     */
+    private $location;
+
+    /**
+     * @var string $description
+     */
+    private $description;
+
+    /**
+     * @var string $how_to_apply
+     */
+    private $how_to_apply;
+
+    /**
+     * @var string $token
+     */
+    private $token;
+
+    /**
+     * @var boolean $is_public
+     */
+    private $is_public;
+
+    /**
+     * @var boolean $is_activated
+     */
+    private $is_activated;
+
+    /**
+     * @var string $email
+     */
+    private $email;
+
+    /**
+     * @var datetime $expires_at
+     */
+    private $expires_at;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string 
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set logo
+     *
+     * @param string $logo
+     */
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+    }
+
+    /**
+     * Get logo
+     *
+     * @return string 
+     */
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set position
+     *
+     * @param string $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * Get position
+     *
+     * @return string 
+     */
+    public function getPosition()
+    {
+        return $this->position;
+    }
+
+    /**
+     * Set location
+     *
+     * @param string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * Get location
+     *
+     * @return string 
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set how_to_apply
+     *
+     * @param string $howToApply
+     */
+    public function setHowToApply($howToApply)
+    {
+        $this->how_to_apply = $howToApply;
+    }
+
+    /**
+     * Get how_to_apply
+     *
+     * @return string 
+     */
+    public function getHowToApply()
+    {
+        return $this->how_to_apply;
+    }
+
+    /**
+     * Set token
+     *
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get token
+     *
+     * @return string 
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Set is_public
+     *
+     * @param boolean $isPublic
+     */
+    public function setIsPublic($isPublic)
+    {
+        $this->is_public = $isPublic;
+    }
+
+    /**
+     * Get is_public
+     *
+     * @return boolean 
+     */
+    public function getIsPublic()
+    {
+        return $this->is_public;
+    }
+
+    /**
+     * Set is_activated
+     *
+     * @param boolean $isActivated
+     */
+    public function setIsActivated($isActivated)
+    {
+        $this->is_activated = $isActivated;
+    }
+
+    /**
+     * Get is_activated
+     *
+     * @return boolean 
+     */
+    public function getIsActivated()
+    {
+        return $this->is_activated;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set expires_at
+     *
+     * @param datetime $expiresAt
+     */
+    public function setExpiresAt($expiresAt)
+    {
+        $this->expires_at = $expiresAt;
+    }
+
+    /**
+     * Get expires_at
+     *
+     * @return datetime 
+     */
+    public function getExpiresAt()
+    {
+        return $this->expires_at;
+    }
+}
